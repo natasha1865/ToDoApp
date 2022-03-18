0801f53f6fdc3e550d8ab49c5c6f0b7f00c6a7be
natasha1865 <natasha-tania1@hotmail.com>
2022-03-19 00:01:08 +0200

    HTML, CSS, JS
    Add new CSS Var, css stylesheet, new func on js themes

--- static/js/app.js
+++ static/js/app.js
@@ -1,54 +1,43 @@
-// Selectors
-const spidermanTheme = document.querySelector('.spiderman');
-const frozenTheme = document.querySelector('.frozen');
-const pirateTheme = document.querySelector('.pirate');
-const outerspaceTheme = document.querySelector('.outerspace');
-const dinosaurTheme = document.querySelector('.dinosaur');
-const unicornTheme = document.querySelector('.unicorn');
+//changing between themes
+let switches = document.getElementsByClassName('switch');
+let style = localStorage.getItem('style');
 
-// Event Listeners to change theme
-spidermanTheme.addEventListener('click', () => changeTheme1('spiderman'));
-frozenTheme.addEventListener('click', () => changeTheme2('frozen'));
-pirateTheme.addEventListener('click', () => changeTheme3('pirate'));
-outerspaceTheme.addEventListener('click', () => changeTheme4('outerspace'));
-dinosaurTheme.addEventListener('click', () => changeTheme5('dinosaur'));
-unicornTheme.addEventListener('click', () => changeTheme6('unicorn'));
-
-// Function to change theme
-function changeTheme1() {
-  document.body.style.backgroundImage = "url('./static/images/spiderman.jpg')";
-  document.body.style.backgroundRepeat = "no-repeat";
-  document.body.style.backgroundSize = "cover";
+if (style == null) {
+  setTheme('standard');
+} else {
+  setTheme(style);
 }
 
-function changeTheme2() {
-  document.body.style.backgroundImage = "url('./static/images/frozen-2.jpg')";
-  document.body.style.backgroundRepeat = "no-repeat";
-  document.body.style.backgroundSize = "cover";
+for (let i of switches) {
+  i.addEventListener('click', function () {
+    let theme = this.dataset.theme;
+    setTheme(theme);
+  });
 }
 
-function changeTheme3() {
-  document.body.style.backgroundImage = "url('./static/images/Pirate-Picture.jpg')";
-  document.body.style.backgroundRepeat = "no-repeat";
-  document.body.style.backgroundSize = "cover";
-}
-
-function changeTheme4() {
-  document.body.style.backgroundImage = "url('./static/images/space.jpg')";
-  document.body.style.backgroundRepeat = "no-repeat";
-  document.body.style.backgroundSize = "cover";
-}
-
-function changeTheme5() {
-  document.body.style.backgroundImage = "url('./static/images/dinosaur.jpg')";
-  document.body.style.backgroundRepeat = "no-repeat";
-  document.body.style.backgroundSize = "cover";
-}
-
-function changeTheme6() {
-  document.body.style.backgroundImage = "url('./static/images/unicorn.jpg')";
-  document.body.style.backgroundRepeat = "no-repeat";
-  document.body.style.backgroundSize = "cover";
+function setTheme(theme) {
+  if (theme == 'standard') {
+    document.getElementById('switcher-id').href = './themes/standard.css';
+  } else if (theme == 'spiderman') {
+    document.getElementById('switcher-id').href = './themes/spiderman.css';
+      let style = document.body.style.backgroundImage = "url('./static/images/spiderman.jpg')";
+  } else if (theme == 'frozen') {
+    document.getElementById('switcher-id').href = './themes/frozen.css';
+      let style = document.body.style.backgroundImage = "url('./static/images/frozen-2.jpg')";
+  } else if (theme == 'pirate') {
+    document.getElementById('switcher-id').href = './themes/pirate.css';
+      let style = document.body.style.backgroundImage = "url('./static/images/Pirate-Picture.jpg')";;
+  } else if (theme == 'outerspace') {
+    document.getElementById('switcher-id').href = './themes/outerspace.css';
+    let style = document.body.style.backgroundImage = "url('./static/images/space.jpg')";
+  } else if (theme == 'dinosaur') {
+    document.getElementById('switcher-id').href = './themes/dinosaur.css';
+    let style = document.body.style.backgroundImage = "url('./static/images/dinosaur.jpg')";
+  } else if (theme == 'unicorn') {
+    document.getElementById('switcher-id').href = './themes/unicorn.css';
+    let style = document.body.style.backgroundImage = "url('./static/images/unicorn.jpg')";
+  } 
+  localStorage.setItem('style', theme);
 }
 
 //ToDo app selectors
