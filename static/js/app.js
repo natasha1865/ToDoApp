5e929d74948fa5b9a2c1f5c61880d6f72fb082fb
natasha1865 <natasha-tania1@hotmail.com>
2022-03-21 11:26:55 +0200

    css
    add new theme styles

--- static/js/app.js
+++ static/js/app.js
@@ -18,15 +18,16 @@ for (let i of switches) {
 function setTheme(theme) {
   if (theme == 'standard') {
     document.getElementById('switcher-id').href = './themes/standard.css';
+    //let style = document.body.style.backgroundImage = './themes/standard.css';
   } else if (theme == 'spiderman') {
     document.getElementById('switcher-id').href = './themes/spiderman.css';
-      let style = document.body.style.backgroundImage = "url('./static/images/spiderman.jpg')";
+    let style = document.body.style.backgroundImage = "url('./static/images/spiderman.jpg')";
   } else if (theme == 'frozen') {
     document.getElementById('switcher-id').href = './themes/frozen.css';
-      let style = document.body.style.backgroundImage = "url('./static/images/frozen-2.jpg')";
+    let style = document.body.style.backgroundImage = "url('./static/images/frozen-2.jpg')";
   } else if (theme == 'pirate') {
     document.getElementById('switcher-id').href = './themes/pirate.css';
-      let style = document.body.style.backgroundImage = "url('./static/images/Pirate-Picture.jpg')";;
+    let style = document.body.style.backgroundImage = "url('./static/images/Pirate-Picture.jpg')";;
   } else if (theme == 'outerspace') {
     document.getElementById('switcher-id').href = './themes/outerspace.css';
     let style = document.body.style.backgroundImage = "url('./static/images/space.jpg')";
